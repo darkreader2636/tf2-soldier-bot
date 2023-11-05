8dee4c6c48dd1c0fe833fbbc8b8e17f05a2fefb1
darkreader2636 <119101878+darkreader2636@users.noreply.github.com>
2023-11-05 17:44:02 +0300

    Anti Profanity Fixed

--- TF2Soldier.py
+++ TF2Soldier.py
@@ -4,11 +4,14 @@ import discord
 from discord.ext import commands
 import random
 import os
-import http.client
-import json
 from discord.ext import tasks
 from dotenv import load_dotenv
+import requests
+from bs4 import BeautifulSoup
 import datetime
+from deep_translator import GoogleTranslator
+import pickle
+import asyncio
 
 
 intents = discord.Intents.default()
@@ -21,19 +24,25 @@ bot = commands.Bot(command_prefix='.tf2 ', description=description, intents=inte
 
 load_dotenv()
 
+#static vars
+
+global response
+with open('response.pkl', 'rb') as f:
+    response = pickle.load(f)
+
+global badwords
 with open('karaliste.txt', 'r') as f:
     words = f.read()
     badwords = words.splitlines()
 
-#static vars
-response = {
-  "gmod": "<@1008352427885465691> <@998201530639470642> <@831138134669918238> gmod girek."
-}
-
 img_response = {
-  "buneamk": "./bunemm.jpg",
-  "kahkaha": "./kahkaha.gif",
-  "alperinbacusu": "./onay.jpg"
+  "buneamk": "./images/bunemm.jpg",
+  "buneamk 2": "./images/buneamk2.jpg",
+  "kahkaha": "./images/kahkaha.gif",
+  "alperinbacusu": "./images/onay.jpg",
+  "salih": "./images/salih.mp4",
+  "ney": "./images/ney.jpg",
+  "al oc": "./images/parmak.mp4"
 }
 
 memes = os.listdir("./memes/")
@@ -41,26 +50,29 @@ shitposts = os.listdir("./shitpost/")
 hl_shitposts = os.listdir("./hl1/")
 stopper = 0
 
-def namazvakit():
-    conn = http.client.HTTPSConnection("api.collectapi.com")
+def contains_word(s, w):
+    return (' ' + w + ' ') in (' ' + s + ' ')
 
-    headers = {
-        'content-type': "application/json",
-        'authorization': "apikey 4tgdakJZiRJ8NBGPXpEdQ0:1zRXT1cRU9OZYBUQJnplrg"
-        }
-
-    conn.request("GET", "/pray/all?data.city=kastamonu", headers=headers)
-
-    res = conn.getresponse()
-    data = res.read()
-    return data.decode("utf-8")
-
-def pp_json(json_thing, sort=True, indents=4):
-    if type(json_thing) is str:
-        return json.dumps(json.loads(json_thing), sort_keys=sort, indent=indents, ensure_ascii=False)
-    else:
-        return json.dumps(json_thing, sort_keys=sort, indent=indents)
-    return None
+def namazgonder():
+    r = requests.get("https://namazvakitleri.diyanet.gov.tr/tr-TR/9609/kastamonu-icin-namaz-vakti")
+    source = BeautifulSoup(r.content,"lxml")
+    tarih = source.find("div",attrs={"class":"ti-hicri"})
+    tarih = tarih.text
+    tarih = tarih.replace("\n","")
+    imsak = source.find("div",attrs={"data-vakit-name":"imsak"}).find("div",attrs={"class":"tpt-time"}).text
+    imsak = "İmsak Vakti : "+imsak+"\n"
+    gunes = source.find("div",attrs={"data-vakit-name":"gunes"}).find("div",attrs={"class":"tpt-time"}).text
+    gunes = "Güneş Vakti : "+gunes+"\n"
+    oglen = source.find("div",attrs={"data-vakit-name":"ogle"}).find("div",attrs={"class":"tpt-time"}).text
+    oglen = "Öğlen Vakti : "+oglen+"\n"
+    ikindi = source.find("div",attrs={"data-vakit-name":"ikindi"}).find("div",attrs={"class":"tpt-time"}).text
+    ikindi = "İkindi Vakti : "+ikindi+"\n"
+    aksam = source.find("div",attrs={"data-vakit-name":"aksam"}).find("div",attrs={"class":"tpt-time"}).text
+    aksam = "Akşam Vakti : "+aksam+"\n"
+    yatsi = source.find("div",attrs={"data-vakit-name":"yatsi"}).find("div",attrs={"class":"tpt-time"}).text
+    yatsi = "Yatsı Vakti : "+yatsi+"\n"
+    vakit = imsak+gunes+oglen+ikindi+aksam+yatsi
+    return vakit
 
 @bot.event
 async def on_ready():
@@ -71,6 +83,47 @@ async def on_ready():
     print(datetime.datetime.now())
     kalan_gun.start()
     await bot.tree.sync()
+
+@bot.event
+async def on_message(message: discord.Message):
+    channel = bot.get_channel(1156596803291058227)
+    if message.author == bot.user:
+        return
+    if message.guild is None and not message.author.bot:
+        await channel.send(message.content)
+    if contains_word(message.content.lower(), "sa") and not  message.author.bot:
+        await message.channel.send("as")
+    if contains_word(message.content.lower(), "ney") and not  message.author.bot:
+        await message.channel.send(file=discord.File(img_response["ney"]))
+    await kufur_kontrol(message, message.author)
+    await bot.process_commands(message)
+
+async def kufur_kontrol(message, user):
+    global badwords
+    msg = message.content.lower()
+    user = message.author
+    for word in badwords:
+        if word in msg:
+            if contains_word(msg, word):
+                channel = bot.get_channel(1169648813414305833)
+                embed=discord.Embed(title="Küfür Kaydı", color=0xff0000)
+                embed.add_field(name="Kullanıcı", value = message.author, inline=True)
+                embed.add_field(name="Kanal", value = message.channel, inline=True)
+                embed.add_field(name="Mesaj", value = message.content, inline=False)
+                embed.add_field(name="Yasaklı Kelime", value = word, inline=True)
+                embed.add_field(name="Tarih", value=str(datetime.datetime.now()), inline=True)
+                await channel.send(embed=embed)
+                dm_embed = discord.Embed(title="Küfür Kaydı", color=0xff0000)
+                dm_embed.add_field(name="Sunucu", value = message.guild, inline=True)
+                dm_embed.add_field(name="Kanal", value = message.channel, inline=True)
+                dm_embed.add_field(name="Mesaj", value = message.content, inline=False)
+                dm_embed.add_field(name="Yasaklı Kelime", value = word, inline=True)
+                dm_embed.add_field(name="Tarih", value=str(datetime.datetime.now()), inline=True)
+                await user.send(embed=dm_embed)
+                await asyncio.sleep(0.5)
+                await message.delete()
+                break
+    return
 
 @tasks.loop(minutes=1)
 async def kalan_gun():
@@ -88,18 +141,43 @@ async def add(ctx, left: int, right: int):
     """İki sayıyı toplar. (add <sayı> <sayı>)"""
     await ctx.send(left + right)
 
+@bot.hybrid_command()
+async def çevir(ctx, istek: str, hedef: str):
+    translated = GoogleTranslator(source='auto', target=hedef).translate(text=istek)  
+    await ctx.send(translated)
 
-@bot.command(pass_context=True)
+
+@bot.hybrid_command(pass_context=True)
 async def ping(ctx):
     """ Pong! """
     await ctx.send('Pong! {0}'.format(round(bot.latency, 1)))
+
+@bot.hybrid_command()
+async def karaliste_ekle(ctx, kelime: str):
+    global badwords
+    with open('karaliste.txt', 'a') as f:
+        f.write("{0}\n".format(kelime))
+    with open('karaliste.txt', 'r') as f:
+        words = f.read()
+        badwords = words.splitlines()
+    await ctx.send("Eklendi!")
  
-@bot.command()
+@bot.hybrid_command()
 async def hızlı(ctx, rp: str ):
     """   Hızlı yanıt modu. (hızlı <mesaj>)"""
     await ctx.send(response[rp])
 
-@bot.command()
+@bot.hybrid_command()
+async def hızlı_ekle(ctx, name: str , tmp: str):
+    global response
+    if name not in response.keys():
+        response[name] = tmp
+    with open('response.pkl', 'wb') as f:
+        pickle.dump(response, f)
+    with open('response.pkl', 'rb') as f:
+        response = pickle.load(f)
+
+@bot.hybrid_command()
 async def meme(ctx):
     """Rastgele bir resim gönderir."""
     selected_file = random.choice(memes)
@@ -107,7 +185,7 @@ async def meme(ctx):
     print("Sending: ", path)
     await ctx.send(file=discord.File(path))
 
-@bot.command()
+@bot.hybrid_command()
 async def alper(ctx, istek: str):
     if ( ctx.guild.id == 1122457879660724265):
         """Alperin söylediği efsane şarkıları yollar. (pepe, kısa, uzun)""",
@@ -117,8 +195,8 @@ async def alper(ctx, istek: str):
     else:
         await ctx.send("Bilinmeyen Komut")
 
-@bot.command()
-async def repeat(ctx, times: int, *, content='repeating...'):
+@bot.hybrid_command()
+async def repeat(ctx, times: int, *, content):
     """Bir mesajı tekrarlar. (repeat <miktar> <mesaj>)"""
     global stopper
     if stopper:
@@ -133,18 +211,18 @@ async def repeat(ctx, times: int, *, content='repeating...'):
         await ctx.send(content)
     return
     
-@bot.command()
+@bot.hybrid_command()
 async def namaz(ctx):
     """Namaz vakitlerini gösterir."""
-    await ctx.send(pp_json(namazvakit()))
+    await ctx.send(namazgonder())
 
-@bot.command()
+@bot.hybrid_command()
 async def dur(ctx):
     """Repeat komutunu durdurur."""
     global stopper
     stopper = True
 
-@bot.command()
+@bot.hybrid_command()
 async def resim(ctx, *, resim):
     """İsmi verilen resmi gönderir"""
     print("Sending: {}".format(resim))
@@ -158,7 +236,7 @@ async def resim(ctx, *, resim):
     await ctx.message.delete()
     
     
-@bot.command()
+@bot.hybrid_command()
 async def shitpost(ctx):
     """Rastgele bir shitpost gönderir."""
     selected_sp = random.choice(shitposts)
@@ -166,7 +244,7 @@ async def shitpost(ctx):
     print("Sending: Shitpost ", patsp)
     await ctx.send(file=discord.File(patsp))
 
-@bot.command()
+@bot.hybrid_command()
 async def hl_shitpost(ctx):
     """Rastgele bir Half-Life shitpostu gönderir."""
     selected_hl = random.choice(hl_shitposts)
